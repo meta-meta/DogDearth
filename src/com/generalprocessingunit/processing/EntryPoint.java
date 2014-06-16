b57ec2d4f05d1a760c655a1667634787ea8750a3
meta-meta <paulmchristian@gmail.com>
2014-06-15 20:59:42 -0400

    rename Main to EntryPoint and EntryPoint to EntryPointVR

--- src/com/generalprocessingunit/processing/EntryPoint.java
+++ src/com/generalprocessingunit/processing/EntryPoint.java
@@ -1,60 +1,57 @@
 package com.generalprocessingunit.processing;
 
-import com.generalprocessingunit.vr.PAppletVR;
+import processing.core.PApplet;
 import processing.core.PGraphics;
-import processing.core.PVector;
 
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
-public class EntryPoint extends PAppletVR
-{
+public class EntryPoint extends PApplet{
 
     DogDearth dogDearth = new DogDearth();
+    PGraphics pG;
+
 
     public static void main(String[] args){
-        PAppletVR.main(new EntryPoint());
-    }
+		PApplet.main(new String[] { /*"--present",*/ EntryPoint.class.getCanonicalName() });
+	}
 
 
-    @Override
-    public void setup() {
-        super.setup();
+	@Override
+	public void setup() {		
+		size(1400, 900, P3D);
+        pG = createGraphics(width,height, P3D);
         dogDearth.setup(this);
     }
 
-    @Override
-    public void beforeDraw() {
-        super.beforeDraw();
+	
+	@Override
+	public void draw(){
         dogDearth.beforeDraw(this);
+
+        pG.beginDraw();
+        pG.camera();
+        pG.perspective();
+        pG.hint(DISABLE_DEPTH_MASK);
+        dogDearth.draw2D(pG);
+        pG.hint(ENABLE_DEPTH_MASK);
+
+        pG.camera(0,0,0, dogDearth.player.lookAt.x, dogDearth.player.lookAt.y, dogDearth.player.lookAt.z, 0, 1f, 0);
+//        pG.camera(dogDearth.playerLocation.x, dogDearth.playerLocation.y, dogDearth.playerLocation.z, dogDearth.playerLocation.x + dogDearth.player.lookAt.x, dogDearth.playerLocation.y + dogDearth.player.lookAt.y, dogDearth.playerLocation.z + dogDearth.player.lookAt.z, 0, -1f, 0);
+        pG.perspective(PI / 2.8f, width / height, 0.1f, 10000f);
+
+        dogDearth.drawScene(pG, this);
+
+        pG.endDraw();
+        image(pG, 0, 0);
     }
 
     @Override
-    public void draw() {
-        super.draw();
-    }
-
-    @Override
-    public void drawInitialScene(PGraphics scene)
+    public void mousePressed(MouseEvent e)
     {
-        super.drawInitialScene(scene);
-    }
-
-    @Override
-    public void draw2dScene(PGraphics scene, int eye, PVector hydraBasePos)
-    {
-        dogDearth.draw2D(scene);
-    }
-
-
-
-    @Override
-    public void drawScene(PGraphics scene, int eye, PVector hydraBasePos) {
-
-//        pG.camera(dogDearth.playerLocation.x, dogDearth.playerLocation.y, dogDearth.playerLocation.z, dogDearth.playerLocation.x + dogDearth.lookAt.x, dogDearth.playerLocation.y + dogDearth.lookAt.y, dogDearth.playerLocation.z + dogDearth.lookAt.z, 0, -1f, 0);
-//        pG.perspective(PI / 2.8f, width / height, 0.1f, 10000f);
-
-        dogDearth.drawScene(scene, this);
-
+        dogDearth.mouseXAtClick = e.getX();
+        dogDearth.mouseYAtClick = e.getY();
+        super.mousePressed(e);
     }
 
     @Override
@@ -67,9 +64,5 @@ public class EntryPoint extends PAppletVR
     public void keyReleased(KeyEvent e)
     {
         DogDearth.keys.put(e.getKeyCode(), false);
-
-        if(e.getKeyCode() == ESC){
-            exit();
-        }
     }
 }
---
+++ src/com/generalprocessingunit/processing/EntryPointVR.java
@@ -0,0 +1,75 @@
+package com.generalprocessingunit.processing;
+
+import com.generalprocessingunit.vr.PAppletVR;
+import processing.core.PGraphics;
+import processing.core.PVector;
+
+import java.awt.event.KeyEvent;
+
+public class EntryPointVR extends PAppletVR
+{
+
+    DogDearth dogDearth = new DogDearth();
+
+    public static void main(String[] args){
+        PAppletVR.main(new EntryPointVR());
+    }
+
+
+    @Override
+    public void setup() {
+        super.setup();
+        dogDearth.setup(this);
+    }
+
+    @Override
+    public void beforeDraw() {
+        super.beforeDraw();
+        dogDearth.beforeDraw(this);
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+
+    @Override
+    public void drawInitialScene(PGraphics scene)
+    {
+        super.drawInitialScene(scene);
+    }
+
+    @Override
+    public void draw2dScene(PGraphics scene, int eye, PVector hydraBasePos)
+    {
+        dogDearth.draw2D(scene);
+    }
+
+
+
+    @Override
+    public void drawScene(PGraphics scene, int eye, PVector hydraBasePos) {
+
+//        pG.camera(dogDearth.playerLocation.x, dogDearth.playerLocation.y, dogDearth.playerLocation.z, dogDearth.playerLocation.x + dogDearth.lookAt.x, dogDearth.playerLocation.y + dogDearth.lookAt.y, dogDearth.playerLocation.z + dogDearth.lookAt.z, 0, -1f, 0);
+//        pG.perspective(PI / 2.8f, width / height, 0.1f, 10000f);
+
+        dogDearth.drawScene(scene, this);
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        DogDearth.keys.put(e.getKeyCode(), true);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+        DogDearth.keys.put(e.getKeyCode(), false);
+
+        if(e.getKeyCode() == ESC){
+            exit();
+        }
+    }
+}
